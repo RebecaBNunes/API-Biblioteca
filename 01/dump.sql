bc353b34bc57a9f3379df79568740ede92a2cffd
Rebeca Barbosa Nunes da Silva <beca9375@gmail.com>
2023-09-06 12:23:25 -0300

    criada a tabela de livros

--- 01/dump.sql
+++ 01/dump.sql
@@ -5,3 +5,12 @@ CREATE TABLE autores (
   nome VARCHAR(150) NOT NULL,
   idade INTEGER
 );
+
+CREATE TABLE livros (
+	id SERIAL PRIMARY KEY,
+  nome VARCHAR(150) NOT NULL,
+  genero VARCHAR(80),
+  editora VARCHAR(100),
+  data_publicacao VARCHAR(20),
+  autor_id INTEGER REFERENCES autores(id)
+);
